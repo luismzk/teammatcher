c198e87a45e6147c464908b5d57ecae2b63f1d03
Luis I. Martinez K <luismzk@gmail.com>
2020-02-04 21:38:13 -0400

    Post for sorting (will handle server side). copy sorted teams to clipboard. Toggle show teams when sorted

--- routes/landing/homepage.js
+++ routes/landing/homepage.js
@@ -11,15 +11,50 @@ router.get('/', async function (req, res) {
       host: req.headers.host,
       user: req.user,
       levels,
-      teams
+      teams,
+      teamNumber: teams.length
     });
 
-
   } catch (e) {
+
     console.log(e);
-    //res.redirect('/404');
     res.send("error")
+
   }
 });
 
+router.post('/sortTeams', async function (req, res) {
+  try {
+    var array1 = ["a"];
+    var array2 = ["b"];
+    var array3 = ["c"];
+    console.log(req.body);
+    
+    res.send({ team1: array1, team2: array2, team3: array3 });
+
+
+  } catch (e) {
+
+    console.log(e);
+    res.sendStatus(500);
+
+  }
+});
+
+
+function pushToTeam(player, teamIndex){
+
+    switch (teamIndex){
+      case 0:
+        team1.push(player);
+        break;
+      case 1:
+        team2.push(player);
+        break;
+      case 2:
+        team3.push(player);
+        break;
+    }
+
+}
 module.exports = router;
