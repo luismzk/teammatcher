486e0f9a189ef4b1ddbe790babb8a4697923ba42
Luis I. Martinez K <luismzk@gmail.com>
2020-03-05 19:21:13 -0400

    New file for sorting. Changed get to post. New functions

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ module.exports = (app, passport) => {
 
 
   app.use(require('./routes/landing/homepage'));
+  app.use(require('./routes/team_handler/team_sorting'));
 
 
 }
--- routes/landing/homepage.js
+++ routes/landing/homepage.js
@@ -23,46 +23,4 @@ router.get('/', async function (req, res) {
   }
 });
 
-router.post('/sortTeams', async function (req, res) {
-
-  try {
-
-    var array1 = ["a"];
-    var array2 = ["b", "d"];
-    var array3 = ["c"];
-
-    // Array con arrays, aqui se guardaran todos los equipos
-    var teamArray = [ array1, array2, array3];
-
-    // Sorteará los equipos para tener los que tengan menos integrantes de primero y llenarlos
-    teamArray.sort((a, b) => Number(a.length) - Number(b.length));
-
-    console.log(teamArray);
-    res.send({ team1: array1, team2: array2, team3: array3 });
-
-  } catch (e) {
-
-    console.log(e);
-    res.sendStatus(500);
-
-  }
-
-});
-
-
-function pushToTeam(player, teamIndex){
-
-    switch (teamIndex){
-      case 0:
-        team1.push(player);
-        break;
-      case 1:
-        team2.push(player);
-        break;
-      case 2:
-        team3.push(player);
-        break;
-    }
-
-}
 module.exports = router;
