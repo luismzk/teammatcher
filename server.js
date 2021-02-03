41f8c7154fd9b806a891e1d7d1abca6dd187fe88
Luis I. Martinez K <luismzk@gmail.com>
2021-02-03 19:09:12 -0400

    minor stuff

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 require('./config/config');
 
-// const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const hbs = require('hbs');
-// const fs = require('fs');
 
 const passport = require('passport');
-// const flash = require('connect-flash');
-// const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000; // LocalHost Port
-// var Hashids = require('hashids');
-//const session = require('express-session');
-// const path = require('path');
-// const multer = require('multer');
- var {mongoose} = require('./db/mongoose');
-// var nodemailer = require('nodemailer');
-var helpers = require('handlebars-helpers')();
-var handlebars = require('handlebars');
-// var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
-// require('./middleware/passport')(passport);
+const { mongoose } = require('./db/mongoose');
+const helpers = require('handlebars-helpers')();
+const handlebars = require('handlebars');
 
 var app = express();
 
@@ -47,21 +36,6 @@ hbs.registerHelper(handlebars);
 
 app.use(express.static('public'));
 
-//app.use(cookieParser());
-/*
-app.use(session({
-	secret: 'secretsalt',
-	resave: false,
-	saveUninitialized: false,
-  cookie: {
-    maxAge: 1000*60*60*24*31
-  }
-}));*/
-/*
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-*/
 // Routes
 require('./routes.js')(app, passport);
 
